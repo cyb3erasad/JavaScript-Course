61cf5674e7a99208d97d86f45772b6959544b34c
cyb3erasad <cyb3rasad@gmail.com>
2026-02-08 22:34:03 +0500

    Learn control flow

---
+++ 04-Control-Flow/truthy.js
@@ -0,0 +1,38 @@
+// falsy values  
+
+// false, 0, -0, BigInt 0n, "", null, NaN, undefined
+
+// truthy values 
+
+// "0", 'false', " ", [], {}, function(){},
+
+const month = []
+
+// if (month.length === 0) {
+//     console.log("array is empty");   
+// } 
+
+const emptyObject = {}
+
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Object is empty");
+    
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let var1;
+// // var1 = 5 ?? 10
+// // var1 = null ?? 10
+// // var1 = undefined ?? 10
+// var1 = null ?? 10 ?? 15
+// console.log(var1);
+
+// terniary operator
+
+// condition ? true : false
+
+const burger = 100
+
+burger >= 50 ? console.log("Greator then 50") : console.log("Less then 50");
+
