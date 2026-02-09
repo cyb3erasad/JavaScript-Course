a2c0386afa20364c0e4bac7d6c41aca701ecaf92
cyb3erasad <cyb3rasad@gmail.com>
2026-02-09 21:26:58 +0500

    learn while and do while loop

---
+++ 05-Iterations/two.js
@@ -0,0 +1,26 @@
+// while loop
+
+// let i = 1
+// while (i <= 10) {
+//     console.log(`Value of i: ${i}`);
+//     i = i + 2
+// }
+
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+let arr = 0
+
+// while (arr < days.length) {
+//     console.log(`value is ${days[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// Do while loop
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score = score + 1
+} while (score <= 10);
